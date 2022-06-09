fff197dda48aaf7d571224c11013a8c7915cfe82
haru <haruki1472@icloud.com>
2022-06-09 23:54:08 +0900

    トップページ作成 mui/導入

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
-FROM node:16.13.0-alpine
-WORKDIR /src
+# FROM node:16.13.0-alpine
+# WORKDIR /src
+FROM node:16.13.0-alpine3.11
+WORKDIR /usr/src/app
---
+++ app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Box, Button } from "@material-ui/core";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        Todoアプリケーションへ移動
+      </Button>
+    </div>
+  );
+}
+
+export default App;
